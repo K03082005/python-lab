47262f249e71fe5b20ed72ca21c729903bd2f92f
K03082005 <kritikhanijo@gmail.com>
2024-10-11 10:02:57 +0530

    Add files via upload

---
+++ lab7.2.py
@@ -0,0 +1,10 @@
+
+class Student:
+    def __init__(self, name, roll_no):
+        self.name = name
+        self.roll_no = roll_no
+
+x = Student("John", 2)
+
+
+print(f"Name: {x.name}, Roll No: {x.roll_no}")
